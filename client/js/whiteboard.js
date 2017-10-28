ce34f70e2bba24637877ea3d7eef215cfd51dee1
Tim <timbartrum73@gmail.com>
2017-10-28 16:52:06 +0100

    Added prompt for name

--- client/js/whiteboard.js
+++ client/js/whiteboard.js
@@ -1,9 +1,38 @@
 $(document).ready(function() {
+	var socket = io();
 
-	$("#whiteboard").mousedown(function() {
-		
+
+	var url = window.location.href;
+	var name = findGetParameter("name");
+	if(name === null) {
+		name = prompt("Enter a username", "");
+		socket.emit("setUsername", name);
+	}
+
+
+	socket.on("invalidName", function(err) {
+		name = prompt("Error: " + err + " Enter a username", "");
+		socket.emit("setUsername", name);
 	});
 
 
+	$("#whiteboard").mousedown(function() {
+
+	});
+
+
+
+	function findGetParameter(parameterName) {
+	    var result = null,
+	        tmp = [];
+	    location.search
+	        .substr(1)
+	        .split("&")
+	        .forEach(function (item) {
+	          tmp = item.split("=");
+	          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+	        });
+	    return result;
+	}
 
 });
