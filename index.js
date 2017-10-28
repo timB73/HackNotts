338b90e07167f6921d73913e9d72a74220fa39f9
Tim <timbartrum73@gmail.com>
2017-10-28 12:46:21 +0100

    awefwea

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var users = {}
+var users = {};
 
 
 app.get('/', function(req, res){
