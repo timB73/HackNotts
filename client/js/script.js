5794dbe2cdcfaa60d1b4df1253eee3baffdc0196
Tim <timbartrum73@gmail.com>
2017-10-28 12:56:07 +0100

    Created page structure & directories

---
+++ client/js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+	var socket = io();
+
+	// tmp set username
+	var name = prompt("Please enter a username", "");
+	socket.emit('setUsername', name);
+	socket.on('invalidName', function(err){
+		name = prompt(err+" Please enter a username", "");
+		socket.emit('setUsername', name);
+	});
+
+	socket.on('validName', function(valName){
+		name = valName;
+		alert("Name "+name+" accepted.");
+	});
+
+});
