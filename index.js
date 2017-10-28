49253694dee66295d81393549345f71003fbf050
sammythew <sammythew@users.noreply.github.com>
2017-10-28 12:24:25 +0100

    added server-side username support

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users = {}
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+
+
 io.on('connection', function(socket){
-	
+
+	socket.on('setUsername', function(name){
+		if(users[name]){
+			socket.emit('invalidName', "Username taken.");
+		} else if(name.trim() == "") {
+			socket.emit('invalidName', "Username must contain non-whitespace characters.");
+		} else {
+			socket.emit('validName', name);
+			io.emit('userJoin', name);
+			users[name] = socket;
+		}
+	});
+
+	socket.on('disconnect', function(){
+		io.emit('userLeave', name);
+		delete users[name];
+	});
+
 });
