80594f7667245c6407dbe1d6ae5fd37919458adb
sammythew <sammythew@users.noreply.github.com>
2017-10-29 02:39:11 +0000

    who knows

--- client/js/toolbar.js
+++ client/js/toolbar.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 	for(var i=0; i<toolNames.length;i++){
-		var $button = $('<button id="close-image" style="top:'+(40+(40*i))+'px;left:5px;"><img src="/icons/'+toolNames[i][1]+'"></button>');
+		var $button = $('<button id="close-image" data-id="'+i+'" style="top:'+(40+(40*i))+'px;left:5px;"><img src="/icons/'+toolNames[i][1]+'"></button>');
 		$button.click(function(){
-			tool = i;
-			console.log(i);
+			tool = $(this).data("id");
+			console.log(tool);
 		});
 		$('#toolbar').append($button);
 	}
