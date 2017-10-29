a456dd3a0ef5a7b43250b7f971057b6928535d8f
Tim <timbartrum73@gmail.com>
2017-10-29 10:12:37 +0000

    Fixed images not showing?

--- client/js/toolbar.js
+++ client/js/toolbar.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	for(var i=0; i<toolNames.length;i++){
-		var $button = $('<button class="close-image" id="toolBarEl'+i+'" data-id="'+i+'" style="top:'+(40+(40*i))+'px;left:5px;"><img src="/icons/'+toolNames[i][1]+'"></button>');
+		var $button = $('<button class="close-image" id="toolBarEl'+i+'" data-id="'+i+'" style="top:'+(40+(40*i))+'px;left:5px;"><img src="icons/'+toolNames[i][1]+'"></button>');
 		$button.click(function(){
 			for(var k=0; k<toolNames.length; k++){
 				$('#toolBarEl'+k).css("background-color", "#E1E1E1");
